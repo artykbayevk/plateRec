4e5fb34bfc762dc7cc90c9c8dbba7b921796ea62
Kamalkhan Artykbayev <kamalsdu@MacBook-Pro-Kamalkhan.local>
2018-05-18 17:19:41 +0600

    static

--- mysite/settings.py
+++ mysite/settings.py
@@ -123,7 +123,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_ROOT = os.path.join(BASE_DIR, '')
 
 STATIC_URL = '/static/'
 
