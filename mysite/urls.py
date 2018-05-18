69e6fd8bc0d26da93687724c622155690b962296
Kamalkhan Artykbayev <kamalsdu@MacBook-Pro-Kamalkhan.local>
2018-05-18 17:21:42 +0600

    asd

--- mysite/urls.py
+++ mysite/urls.py
@@ -18,8 +18,13 @@ from django.contrib import admin
 from django.conf.urls import include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+
 
 urlpatterns = [
     url(r'', include('app.urls')),
     url('admin/', admin.site.urls),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += staticfiles_urlpatterns()
